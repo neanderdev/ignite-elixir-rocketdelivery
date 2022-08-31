0bdecca4b209681b6c4591dd04d551150a68c6e5
neanderdev <neander.faria@hotmail.com>
2022-08-31 10:31:54 -0300

    Criando o schema do User pt 2

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Rocketdelivery.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
