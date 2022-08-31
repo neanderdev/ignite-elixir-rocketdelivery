eece0cccd6f9938ba12a451f1c6f3912d6be9bcc
neanderdev <neander.faria@hotmail.com>
2022-08-31 16:48:31 -0300

    Refatorando os módulos com o uso do Plug

--- lib/rocketdelivery/users/delete.ex
+++ lib/rocketdelivery/users/delete.ex
@@ -1,15 +1,7 @@
 defmodule Rocketdelivery.Users.Delete do
-  alias Ecto.UUID
   alias Rocketdelivery.{Error, Repo, User}
 
   def call(id) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_error()}
-      {:ok, uuid} -> delete(uuid)
-    end
-  end
-
-  defp delete(id) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> Repo.delete(user)
