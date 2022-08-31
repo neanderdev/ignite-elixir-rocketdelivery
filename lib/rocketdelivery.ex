ddc6d48ca7f59e9fdbd190c5fb2da0f55bde779f
neanderdev <neander.faria@hotmail.com>
2022-08-31 16:01:34 -0300

    Criando a rota de deleção de usuários

--- lib/rocketdelivery.ex
+++ lib/rocketdelivery.ex
@@ -1,7 +1,9 @@
 defmodule Rocketdelivery do
   alias Rocketdelivery.Users.Create, as: UserCreate
+  alias Rocketdelivery.Users.Delete, as: UserDelete
   alias Rocketdelivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
--- lib/rocketdelivery_web/controllers/users_controller.ex
+++ lib/rocketdelivery_web/controllers/users_controller.ex
@@ -14,6 +14,14 @@ defmodule RocketdeliveryWeb.UsersController do
     end
   end
 
+  def delete(conn, %{"id" => id}) do
+    with {:ok, %User{}} <- Rocketdelivery.delete_user(id) do
+      conn
+      |> put_status(:no_content)
+      |> text("")
+    end
+  end
+
   def show(conn, %{"id" => id}) do
     with {:ok, %User{} = user} <- Rocketdelivery.get_user_by_id(id) do
       conn
