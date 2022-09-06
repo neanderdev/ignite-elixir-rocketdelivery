36f6629f2fbf4708b1a11dafa53094eca1592c16
neanderdev <neander.faria@hotmail.com>
2022-09-06 14:11:27 -0300

    Evitando chamadas desnecessárias para a API

--- lib/rocketdelivery/user.ex
+++ lib/rocketdelivery/user.ex
@@ -27,6 +27,8 @@ defmodule Rocketdelivery.User do
     timestamps()
   end
 
+  def build(changeset), do: apply_action(changeset, :create)
+
   def changeset(struct \\ %__MODULE__{}, params) do
     struct
     |> cast(params, @required_params)
