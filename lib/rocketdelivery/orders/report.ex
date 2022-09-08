d38921297ba5e720ad2761a9f1398c1e226f5b4a
neanderdev <neander.faria@hotmail.com>
2022-09-08 15:33:01 -0300

    Calculando o preço total e salvando o relatório

--- lib/rocketdelivery/orders/report.ex
+++ lib/rocketdelivery/orders/report.ex
@@ -2,25 +2,34 @@ defmodule Rocketdelivery.Orders.Report do
   import Ecto.Query
 
   alias Rocketdelivery.{Item, Order, Repo}
+  alias Rocketdelivery.Orders.TotalPrice
 
   @default_block_size 500
 
   def create(filename \\ "report.csv") do
     query = from(order in Order, order_by: order.user_id)
 
-    Repo.transaction(fn ->
-      query
-      |> Repo.stream(max_rows: @default_block_size)
-      |> Stream.chunk_every(@default_block_size)
-      |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
-      |> Enum.map(&parse_line/1)
-    end)
+    {:ok, order_list} =
+      Repo.transaction(
+        fn ->
+          query
+          |> Repo.stream(max_rows: @default_block_size)
+          |> Stream.chunk_every(@default_block_size)
+          |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
+          |> Enum.map(&parse_line/1)
+        end,
+        timeout: :infinity
+      )
+
+    File.write(filename, order_list)
   end
 
   defp parse_line(%Order{user_id: user_id, payment_method: payment_method, items: items}) do
+    total_price = TotalPrice.calculate(items)
+
     items_string = Enum.map(items, fn item -> items_string(item) end)
 
-    "#{user_id},#{payment_method},#{items_string}#{50.0}\n"
+    "#{user_id},#{payment_method},#{items_string}#{total_price}\n"
   end
 
   defp items_string(%Item{category: category, description: description, price: price}) do
