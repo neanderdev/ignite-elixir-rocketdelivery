41d4be9052aa952bb5f1d5f39d5c931f5420fb7d
neanderdev <neander.faria@hotmail.com>
2022-09-08 10:50:37 -0300

    Carregando os items em blocos

--- lib/rocketdelivery/orders/report.ex
+++ lib/rocketdelivery/orders/report.ex
@@ -3,13 +3,17 @@ defmodule Rocketdelivery.Orders.Report do
 
   alias Rocketdelivery.{Order, Repo}
 
+  @default_block_size 500
+
   def create(filename \\ "report.csv") do
     query = from(order in Order, order_by: order.user_id)
 
     Repo.transaction(fn ->
       query
-      |> Repo.stream()
-      |> Enum.map(fn item -> item end)
+      |> Repo.stream(max_rows: @default_block_size)
+      |> Stream.chunk_every(@default_block_size)
+      |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
+      |> Enum.into([])
       |> IO.inspect()
     end)
   end
