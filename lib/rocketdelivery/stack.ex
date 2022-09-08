0263c01b074b8f738898744dc3ebbaee56e442cd
neanderdev <neander.faria@hotmail.com>
2022-09-08 15:54:35 -0300

    Implementando funções client na nossa pilha

--- lib/rocketdelivery/stack.ex
+++ lib/rocketdelivery/stack.ex
@@ -1,6 +1,20 @@
 defmodule Rocketdelivery.Stack do
   use GenServer
 
+  # Client
+
+  def start_link(initial_stack) when is_list(initial_stack) do
+    GenServer.start_link(__MODULE__, initial_stack)
+  end
+
+  def push(pid, element) do
+    GenServer.cast(pid, {:push, element})
+  end
+
+  def pop(pid) do
+    GenServer.call(pid, :pop)
+  end
+
   # Server (Callbacks)
 
   @impl true
